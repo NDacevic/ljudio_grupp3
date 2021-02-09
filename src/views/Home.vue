7a09092c6a3e45b2aea84e45afa041cbd7a4eb83
Martin <martin.ekman@dynamicdog.se>
2021-02-09 16:26:09 +0100

    Import of MaterialUI & Example code in centerContent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,12 @@
       <!-- Komponent 1 -->
     </div>
     <div class="centerContent">
-      <p>Center Content</p>
+      <md-tabs>
+        <md-tab id="tab-home" md-label="Home"></md-tab>
+        <md-tab id="tab-pages" md-label="Pages"></md-tab>
+        <md-tab id="tab-posts" md-label="Posts"></md-tab>
+        <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+      </md-tabs>
       <!-- Komponent 2 -->
     </div>
     <div class="rightBarTop">
