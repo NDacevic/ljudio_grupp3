ace609ca6e7eaefb4a55d2234c2990a18091789b
Martin <martin.ekman@dynamicdog.se>
2021-02-09 16:30:08 +0100

    Added semi-kolons

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/store.js'
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store.js';
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
