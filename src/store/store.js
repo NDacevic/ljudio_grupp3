c149e7fc466a2f4b68d425e7f3074a9f1a72ed2e
mikaelollhage <mikaelollhage@gmail.com>
2021-02-09 20:03:23 +0100

    generic fetch for all searches added

--- src/store/store.js
+++ src/store/store.js
@@ -5,10 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    searchResults: []
   },
   mutations: {
+    setSearchResults(state, searchResults) {
+      state.searchResults = searchResults;
+    }
   },
   actions: {
+    async getSearchResults({commit}, media, searchString) {
+      const response = await fetch(`/api/yt/${media}/search+${searchString}`);
+      const searchResults =  await response.json();
+      commit("setSearchResults", searchResults);
+    },
   },
   modules: {
   }
