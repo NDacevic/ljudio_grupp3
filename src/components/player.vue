67ee87865534e95bfbbdf4c0a5a6f14eaae9766c
NDacevic <n.dacevic@gmail.com>
2021-02-10 17:27:11 +0100

    Basic functionality done. Plays one specific video. Updates the progressbar during play. You can click the bar to play from a specific point in video time

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="player">
+<<<<<<< HEAD
     <div id="yt-player" style="display:none"></div>
     <p>{{ videoTitle }}</p>
     <div class="player-seekbar">
-      <input type="range" v-bind:value="this.songDuration" />
+      <input
+        type="range"
+        min="0"
+        v-on:click="playFromTime"
+        v-bind:max="this.songDuration"
+        v-model="this.songProgress"
+      />
+=======
+    <div id="yt-player"></div>
+    <div class="player-seekbar">
+      <input type="range" min="0" max="100" />
+>>>>>>> 41cf501... Fixed conflicts
     </div>
     <figure v-on:click="PlayPrev()">Previous</figure>
     <figure v-on:click="Play()">Play</figure>
     <figure v-on:click="Pause()">Pause</figure>
+<<<<<<< HEAD
     <figure v-on:click="PlayNext()">Next</figure>
+=======
+    <figure v-on:click="PlayPrev()">Next</figure>
+>>>>>>> 41cf501... Fixed conflicts
   </div>
 </template>
 
@@ -17,34 +33,58 @@ export default {
   name: "Player",
   methods: {
     PlayPrev() {
+<<<<<<< HEAD
       window.player.previousVideo();
     },
     Play() {
       let videoId = this.$store.getters.getCurrentSong;
       window.player.loadVideoById(videoId);
       window.player.playVideo();
+      window.player.setVolume(10);
     },
     Pause() {
       window.player.pauseVideo();
     },
     PlayNext() {
       //window.player.nextVideo();
+=======
+      //this.musicPlayer.previousVideo();
+    },
+    Play() {
+      let videoId = "Jb_1A-dz8UQ";
+      window.player.loadVideoById(videoId);
+      window.player.playVideo();
+    },
+    Pause() {
+      //this.musicPlayer.pauseVideo();
+    },
+    PlayNext() {
+      //this.musicPlayer.nextVideo();
+>>>>>>> 41cf501... Fixed conflicts
     },
     getSong() {
       this.currentSong = this.$store.getCurrentSong;
     },
+<<<<<<< HEAD
+    playFromTime: function(event) {
+      window.player.seekTo(event.target.value, true);
+    },
     updateSeekBar() {
-      setInterval(function() {
-        if (window.player.getPlayerState() >= 0) {
-          this.songDuration = window.player.getCurrentTime();
-        }
-      }, 1000);
+      setInterval(
+        function() {
+          this.songDuration = window.player.getDuration();
+          this.songProgress = window.player.getCurrentTime();
+        }.bind(this),
+        1000
+      );
     },
   },
   data() {
     return {
       videoTitle: "An Interesting Title",
       duration: Number,
+      progress: Number,
+      num: 25,
     };
   },
   computed: {
@@ -56,10 +96,25 @@ export default {
         this.duration = value;
       },
     },
+    songProgress: {
+      get: function() {
+        return this.progress;
+      },
+      set: function(value) {
+        this.progress = value;
+      },
+    },
   },
   mounted() {
     this.duration = 0;
     this.updateSeekBar();
+=======
+  },
+  computed: {
+    currentSong: Object,
+    nextSong: Object,
+    previousSong: Object,
+>>>>>>> 41cf501... Fixed conflicts
   },
 };
 </script>
