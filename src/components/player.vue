d2fe585d28b823f09610d217b875515f51d75b4a
NDacevic <n.dacevic@gmail.com>
2021-02-10 18:04:33 +0100

    Capitalized the P in player.vue

--- src/components/player.vue
+++ src/components/player.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="player">
-<<<<<<< HEAD
     <div id="yt-player" style="display:none"></div>
     <p>{{ videoTitle }}</p>
     <div class="player-seekbar">
@@ -11,20 +10,11 @@
         v-bind:max="this.songDuration"
         v-model="this.songProgress"
       />
-=======
-    <div id="yt-player"></div>
-    <div class="player-seekbar">
-      <input type="range" min="0" max="100" />
->>>>>>> 41cf501... Fixed conflicts
     </div>
     <figure v-on:click="PlayPrev()">Previous</figure>
     <figure v-on:click="Play()">Play</figure>
     <figure v-on:click="Pause()">Pause</figure>
-<<<<<<< HEAD
     <figure v-on:click="PlayNext()">Next</figure>
-=======
-    <figure v-on:click="PlayPrev()">Next</figure>
->>>>>>> 41cf501... Fixed conflicts
   </div>
 </template>
 
@@ -33,7 +23,6 @@ export default {
   name: "Player",
   methods: {
     PlayPrev() {
-<<<<<<< HEAD
       window.player.previousVideo();
     },
     Play() {
@@ -47,25 +36,10 @@ export default {
     },
     PlayNext() {
       //window.player.nextVideo();
-=======
-      //this.musicPlayer.previousVideo();
-    },
-    Play() {
-      let videoId = "Jb_1A-dz8UQ";
-      window.player.loadVideoById(videoId);
-      window.player.playVideo();
-    },
-    Pause() {
-      //this.musicPlayer.pauseVideo();
-    },
-    PlayNext() {
-      //this.musicPlayer.nextVideo();
->>>>>>> 41cf501... Fixed conflicts
     },
     getSong() {
       this.currentSong = this.$store.getCurrentSong;
     },
-<<<<<<< HEAD
     playFromTime: function(event) {
       window.player.seekTo(event.target.value, true);
     },
@@ -108,13 +82,6 @@ export default {
   mounted() {
     this.duration = 0;
     this.updateSeekBar();
-=======
-  },
-  computed: {
-    currentSong: Object,
-    nextSong: Object,
-    previousSong: Object,
->>>>>>> 41cf501... Fixed conflicts
   },
 };
 </script>
