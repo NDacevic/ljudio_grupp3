8620d0019cc1d092804af5116c8114eb4f9f68e1
NDacevic <n.dacevic@gmail.com>
2021-02-11 10:28:13 +0100

    added variables for next and previous song. Also changed the setSongToPlay to set the whole object instead of just the ID

--- src/store/store.js
+++ src/store/store.js
@@ -1,36 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     searchResults: [],
     currentSong: "j9V78UbdzWI",
+    nextSong: "",
+    prevSong: "",
     componentToRenderInHomeCenter: "search",
-    queuedTracks: []
+    queuedTracks: [],
   },
   mutations: {
     setSearchResults(state, searchResults) {
       state.searchResults = searchResults;
     },
     setSongToPlay(state, song) {
-      state.currentSong = song.videoId;
+      state.currentSong = song;
     },
     updateQueue(state, newQueue) {
       state.queuedTracks = newQueue;
-    }
+    },
   },
   actions: {
-    async getSearchResults({commit}, payload) {
+    async getSearchResults({ commit }, payload) {
       let response;
       if (payload.media === "playlists") {
-        response = await fetch("our own endpoint towards playlists-table")
+        response = await fetch("our own endpoint towards playlists-table");
+      } else {
+        response = await fetch(
+          `/api/yt/${payload.media}/search+${payload.searchString}`
+        );
       }
-      else {
-        response = await fetch(`/api/yt/${payload.media}/search+${payload.searchString}`);
-      }
-      const searchResults =  await response.json();
+      const searchResults = await response.json();
       commit("setSearchResults", searchResults.content);
     },
   },
@@ -38,7 +41,7 @@ export default new Vuex.Store({
     getSearchContent(state) {
       return state.searchResults;
     },
-    getCurrentSong(state){
+    getCurrentSong(state) {
       return state.currentSong;
     },
     getCenterComponentForHome(state) {
@@ -46,8 +49,7 @@ export default new Vuex.Store({
     },
     getQueuedTracks(state) {
       return state.queuedTracks;
-    }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
