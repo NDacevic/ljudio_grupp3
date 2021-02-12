5b8c089dc0a7bd36218e34033a5ac10628692f7f
NDacevic <n.dacevic@gmail.com>
2021-02-12 11:40:18 +0100

    WiP trying to move the youtube player functionality inside the player component so we can controll the player states

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store/store.js';
-import { MdButton, MdContent, MdTabs, MdTable, MdRipple, MdIcon } from 'vue-material/dist/components';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store/store.js";
+import {
+  MdButton,
+  MdContent,
+  MdTabs,
+  MdTable,
+  MdRipple,
+  MdIcon,
+} from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 Vue.use(MdButton);
 Vue.use(MdContent);
@@ -15,8 +27,18 @@ Vue.use(MdIcon);
 
 Vue.config.productionTip = false;
 
-new Vue({
+var vueApp = new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+  methods: {
+    initYoutube() {
+    },
+  }
+}).$mount("#app");
+
+window.onYouTubeIframeAPIReady = () => {
+  vueApp.initYoutube();
+};
+
+
