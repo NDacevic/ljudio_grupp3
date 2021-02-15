d4b5df60b89ce1a71fe3b5d88f24f2dd0704966e
mikaelollhage <mikaelollhage@gmail.com>
2021-02-15 15:29:14 +0100

    Created left menu component

---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="leftMenu">
+    <div id="notifications">
+      Notifications
+      <button id="newNotifications">2</button>
+    </div>
+
+    <button id="searchButton" @click="renderEmptySearchPageInCenter()">
+      <p>Search</p>
+    </button>
+    <button id="logOutButton">
+      <p>Log out</p>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LeftMenu",
+  methods: {
+    renderEmptySearchPageInCenter() {
+      this.$store.commit("setComponentToRenderInHomeCenter", "search");
+      this.$store.commit("setSearchResults", []);
+      this.$store.commit("setSearchHasBeenPerformed", false);
+    },
+  },
+};
+</script>
