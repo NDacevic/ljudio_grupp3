11f5f1806f47a23fd11716fced989582b5f56e86
NDacevic <n.dacevic@gmail.com>
2021-02-17 11:41:13 +0100

    changed track to media in Search.vue and shareComponent.vue. Added 2 methods to extract the correct value for id and name depending on what object you want to share

--- src/components/ShareComponent.vue
+++ src/components/ShareComponent.vue
@@ -2,7 +2,7 @@
   <div id="shareBackground">
     <diV id="shareContainer">
       <h3>Share</h3>
-      <p v-if="!showMessage">{{ track.artist.name }} - {{ track.name }}</p>
+      <p v-if="!showMessage">{{ mediaName }}</p>
       <p v-else>USERNAME NOT FOUND! TRY AGAIN!</p>
       <input id="userInput" type="text" placeholder="Name of the user" />
       <div class="buttonContainer">
@@ -17,7 +17,7 @@
 export default {
   name: "Share",
   props: {
-    track: Object,
+    media: Object,
   },
   data() {
     return {
@@ -34,6 +34,10 @@ export default {
     async sendShareContent() {
       let userInputText = document.getElementById("userInput").value;
       let id;
+      let mediaType = this.media.type != null ? this.media.type : "playlist";
+      let contentId = this.getContentId(mediaType);
+      let contentName = this.getContentName(mediaType);
+
       if (userInputText !== "") {
         id = await this.checkUser(userInputText);
 
@@ -49,18 +53,60 @@ export default {
             userId: id,
             senderName: this.$store.state.user.Username,
             unread: true,
-            sharedContentId: this.track.videoId,
-            sharedContentType: this.track.type,
-            sharedContentName: this.track.name,
+            sharedContentId: contentId,
+            sharedContentType: mediaType,
+            sharedContentName: contentName,
           };
           this.$store.dispatch("sendNotification", notificationToSend);
           this.$store.commit("setNotificationUser", -1);
-          this.$store.commit("hideShareWindow");
+          this.$store.commit("showShareComponent", false);
         }
+      }
+    },
+    getContentId(type) {
+      if (type === "song" || type === "video") {
+        return this.media.videoId;
+      } else if (type === "album" || type === "artist") {
+        return this.media.browseId;
+      } else {
+        console.log("PLid", this.media.PlaylistId);
+        return this.media.PlaylistId.toString();
+      }
+    },
+    getContentName(type) {
+      if (type != "") {
+        return this.media.name;
+      } else {
+        console.log("Plname", this.media.PlaylistName);
+        return this.media.PlaylistName;
       }
     },
     exitShareComponent() {
       this.$store.commit("showShareComponent", false);
+    },
+  },
+  computed: {
+    mediaName() {
+      let mediaName = "";
+      console.log(this.media);
+      switch (this.media.type) {
+        case "song":
+          mediaName = this.media.artist.name + " - " + this.media.name;
+          break;
+        case "video":
+          mediaName = this.media.author + " - " + this.media.name;
+          break;
+        case "album":
+          mediaName = this.media.artist + " - " + this.media.name;
+          break;
+        case "artist":
+          mediaName = this.media.name;
+          break;
+        case "playlist":
+          mediaName = this.media.PlaylistName;
+          break;
+      }
+      return mediaName;
     },
   },
 };
