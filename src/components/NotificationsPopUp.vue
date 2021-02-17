20fa64e6881bd4be0cc6fe4f13c8203a6cdf7a26
mikaelollhage <mikaelollhage@gmail.com>
2021-02-17 15:54:32 +0100

    functionality bound to all buttons. Added a button for Dismiss notification

--- src/components/NotificationsPopUp.vue
+++ src/components/NotificationsPopUp.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="notificationBackground">
     <div id="notificationsPopUp">
-      <h3>New notifications</h3>
+      <h2>New notifications</h2>
       <div id="scroll" class="md-scrollbar">
-        <div id="notification" v-for="(notification, index) in newNotifications" :key="index">
+        <div id="notification" v-for="notification in newNotifications" :key="notification.Id">
           <p>
-            <b>{{ notification.SenderName }}</b> wants to share this {{ notification.SharedContentType }} with you:
+            {{notification.Id}} <b>{{ notification.SenderName }}</b> wants to share this {{ notification.SharedContentType }} with you:
           </p>
           <p>
             <em>{{ notification.SharedContentName }}</em>
           </p>
-          <div id="actionButtons">
-            <div id="songAndVideoButtons" v-if="notification.SharedContentType === 'song' || notification.SharedContentType === 'video'">
-              <button @click="addTrackToQueueOrPlay(notification, false)">Play now</button>
-              <button @click="addTrackToQueueOrPlay(notification, true)">Add to queue</button>
-            </div>
 
-            <button v-else @click="loadMediaContent(notification)">Load {{ notification.SharedContentType }}</button>
+          <div class="notificationActions" v-if="notification.SharedContentType === 'song' || notification.SharedContentType === 'video'">
+            <button @click="addTrackToQueueOrPlay(notification, false)">Play now</button>
+            <button @click="addTrackToQueueOrPlay(notification, true)">Add to queue</button>
+            <button id="dismissButton" @click="updateNotificationUnread(notification)">Dismiss</button>
+          </div>
+          <div class="notificationActions" v-else>
+            <button @click="loadMediaContent(notification)">Load {{ notification.SharedContentType }}</button>
+            <button id="dismissButton" @click="updateNotificationUnread(notification)">Dismiss</button>
           </div>
         </div>
       </div>
-      <button @click="setRenderNotificationsModalFalse">
-        Close Notifications
+      <button id="closeButton" @click="setRenderNotificationsModalFalse">
+        Close
       </button>
     </div>
   </div>
@@ -36,11 +38,18 @@ export default {
     },
     addTrackToQueueOrPlay(notification, addToQueue) {
       this.$store.dispatch("getMediaObjectAndAddToQueueOrPlay", { addToQueue: addToQueue, sharedContentType: notification.SharedContentType, sharedContentId: notification.SharedContentId });
+      this.$store.dispatch("updateNotificationUnread", notification);
+      this.$store.commit("setRenderNotificationsModal", false)
     },
     loadMediaContent(notification) {
-      console.log(notification.SharedContentId);
       this.$store.dispatch("getMediaContentAndRender", { sharedContentType: notification.SharedContentType, sharedContentId: notification.SharedContentId });
+      this.$store.dispatch("updateNotificationUnread", notification);
+      this.$store.commit("setRenderNotificationsModal", false)
     },
+    updateNotificationUnread(notification)
+    {
+      this.$store.dispatch("updateNotificationUnread", notification);
+    }
   },
   computed: {
     newNotifications() {
